22ceaddde04eb0398aeece05214a18c8823de8f9
manu <manu@local.com>
2022-05-06 19:17:37 +0530

    Auto Push Commit

---
+++ vars/python.groovy
@@ -0,0 +1,18 @@
+def lintCheck() {
+    sh "pylint *.py"
+    sh "echo [[  INFO  ]] : Starting Lint Check for $COMPONENT"
+    sh "echo [[  INFO  ]] : Lint Checks Completed"
+}
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script { lintCheck() }
+                }
+            }
+        } // end of stages
+    }
+}
