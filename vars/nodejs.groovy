f5e7a672b70448a5f2ff91355c6db2885ec47106
manu <manu@local.com>
2022-05-06 17:01:57 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,6 +1,6 @@
 def info(COMPONENT) {
-    sh "echo Starting Lint Check for $COMPONENT"
-    sh "echo Lint Checks Completed"
+    sh "echo [INFO] : Starting Lint Check for $COMPONENT"
+    sh "echo [INFO] : Lint Checks Completed"
 //             sh "npm install jslint"
 //             sh "ls -ltr node_modules/jslint/bin/"
 //             sh "node_modules/jslint/bin/jslint.js server.js"
