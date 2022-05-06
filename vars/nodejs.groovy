3ac2c608d8534ecf18e8bf6931c267d204148a90
manu <manu@local.com>
2022-05-06 16:58:11 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,11 @@
-def info(message, URL) {
-    echo "[Info] $message : $URL"
+def info(COMPONENT) {
+    sh "echo Starting Lint Check for $COMPONENT"
+    sh "echo Lint Checks Completed"
+//             sh "npm install jslint"
+//             sh "ls -ltr node_modules/jslint/bin/"
+//             sh "node_modules/jslint/bin/jslint.js server.js"
 }
 
-info("Hello","yahoo.com")
+info('COMPONENT')
 
 
