a2f37d9af32db4400e4721eaddea11696b4afc17
manu <manu@local.com>
2022-05-07 22:40:34 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,6 +4,12 @@ def lintCheck() {
     sh "echo [[  INFO  ]] : Lint Checks Completed"
 }
 
+def sonarCheck() {
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.8.247:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.login=${SONAR_USR}  -Dsonar.password=${SONAR_PSW}"
+    sh "/bin/sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.8.247 ${COMPONENT}"
+}
+
+
 def call() {
     pipeline {
         agent any
@@ -11,6 +17,11 @@ def call() {
             stage('Lint Checks') {
                 steps {
                     script { lintCheck() }
+                }
+            }
+            stage('SonarScan') {
+                steps {
+                    script { sonarCheck() }
                 }
             }
         } // end of stages
