590a1ad475f6ff041ca4dba56d19eb4c2ab229b0
manu <manu@local.com>
2022-05-07 16:17:24 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,7 @@
 def lintCheck() {
+    sh "whoami"
     sh "pwd && ls -ltr"
+    sh  "ls -ltr ~/"
     sh "~/node_modules/eslint/bin/eslint.js *.js"
     sh "echo [[  INFO  ]] : Starting Lint Check for $COMPONENT"
     sh "echo [[  INFO  ]] : Lint Checks Completed"
