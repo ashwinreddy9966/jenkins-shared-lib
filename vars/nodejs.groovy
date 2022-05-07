19275ffe88275102f5818ef7af3bebfb37e2817d
manu <manu@local.com>
2022-05-07 17:06:39 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,6 +5,7 @@ def lintCheck() {
 
 def sonarCheck() {
     sh "sonar-scanner -Dsonar.host.url=http://172.31.8.247:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.login=${SONAR_USR}  -Dsonar.password=${SONAR_PSW}"
+    sh "/bin/sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.8.247 ${COMPONENT}"
 }
 
 
