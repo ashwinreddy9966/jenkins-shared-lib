1d284a6eb6822c35fb582c9b33712cbbe23fcc03
manu <manu@local.com>
2022-05-07 23:35:58 +0530

    Auto Push Commit

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,5 +1,4 @@
 def lintCheck() {
-    sh "pip3 install pylint"
     sh "pylint payment.py"
     sh "echo [[  INFO  ]] : Starting Lint Check for $COMPONENT"
     sh "echo [[  INFO  ]] : Lint Checks Completed"
