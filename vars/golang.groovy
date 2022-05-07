1b3ce72469e207977bf7f6969db65918f70ab711
manu <manu@local.com>
2022-05-07 23:39:03 +0530

    Auto Push Commit

---
+++ vars/golang.groovy
@@ -0,0 +1,26 @@
+def lintCheck() {
+    sh "echo [[  INFO  ]] : Starting Lint Check for $COMPONENT"
+    sh "echo [[  INFO  ]] : Lint Checks Completed"
+}
+
+def call() {
+    pipeline {
+        agent any
+        environment { SONAR = credentials('SONAR') }
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script { lintCheck() }
+                }
+            }
+            stage('SonarScan') {
+                steps {
+                    script {
+                        env.ARGS="-Dsonar.sources=."
+                        common.sonarCheck() }
+                }
+            }
+
+        } // end of stages
+    }
+}
