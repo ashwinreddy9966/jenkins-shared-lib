d29da412456658f713b8f865720b216bdc64e55a
manu <manu@local.com>
2022-05-11 18:23:06 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -47,7 +47,7 @@ def call() {
                 }
                 steps {
                     script {
-                        def UPLOAD_STATUS=sh(returnStdout: true, script: "curl http://172.31.5.22:8081/service/rest/repository/browse/\${COMPONENT} |grep \${COMPONENT}-\${TAG_NAME}.zip")
+                        def UPLOAD_STATUS=sh(returnStdout: true, script: 'cu -v -s http://172.31.5.22:8081/service/rest/repository/browse/${COMPONENT}'
                         print UPLOAD_STATUS
                     }
 
