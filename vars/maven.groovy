3bc9489e0ef728f38478f4fb007d0d8e84200d12
manu <manu@local.com>
2022-05-11 21:07:58 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -9,44 +9,44 @@ def call() {
     }
 }
 
-
-def call() {
-    pipeline {
-        agent any
-        environment { SONAR = credentials('SONAR') }
-        stages {
-            stage('Lint Checks') {
-                steps {
-                    script { lintCheck() }
-                }
-            }
-            stage('SonarScan') {
-                steps {
-                    script {
-                        env.ARGS="-Dsonar.java.binaries=target/"
-                        common.sonarCheck() }
-                    }
-                }
-            stage('Test Cases') {
-                parallel {
-                    stage('Unit Tests') {
-                            steps {
-                                sh "echo UNIT TESTS Completed"
-                            }
-
-                    }
-                    stage('Integration Tests') {
-                        steps {
-                            sh "echo INTEGRATION TESTS Completed"
-                        }
-                    }
-                    stage('Functional Tests') {
-                        steps {
-                            sh "echo FUNCTIONAL TESTS Completed"
-                        }
-                    }
-                }
-            }
-        } // end of stages
-    }
-}
+//
+//def call() {
+//    pipeline {
+//        agent any
+//        environment { SONAR = credentials('SONAR') }
+//        stages {
+//            stage('Lint Checks') {
+//                steps {
+//                    script { lintCheck() }
+//                }
+//            }
+//            stage('SonarScan') {
+//                steps {
+//                    script {
+//                        env.ARGS="-Dsonar.java.binaries=target/"
+//                        common.sonarCheck() }
+//                    }
+//                }
+//            stage('Test Cases') {
+//                parallel {
+//                    stage('Unit Tests') {
+//                            steps {
+//                                sh "echo UNIT TESTS Completed"
+//                            }
+//
+//                    }
+//                    stage('Integration Tests') {
+//                        steps {
+//                            sh "echo INTEGRATION TESTS Completed"
+//                        }
+//                    }
+//                    stage('Functional Tests') {
+//                        steps {
+//                            sh "echo FUNCTIONAL TESTS Completed"
+//                        }
+//                    }
+//                }
+//            }
+//        } // end of stages
+//    }
+//}
--- vars/python.groovy
+++ vars/python.groovy
@@ -9,43 +9,43 @@ def call() {
     }
 }
 
-def call() {
-    pipeline {
-        agent any
-        environment { SONAR = credentials('SONAR') }
-        stages {
-            stage('Lint Checks') {
-                steps {
-                    script { common.lintCheck() }
-                }
-            }
-            stage('SonarScan') {
-                steps {
-                    script {
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck() }
-                }
-            }
-            stage('Test Cases') {
-                parallel {
-                    stage('Unit Tests') {
-                        steps {
-                            sh "echo UNIT TESTS Completed"
-                        }
-
-                    }
-                    stage('Integration Tests') {
-                        steps {
-                            sh "echo INTEGRATION TESTS Completed"
-                        }
-                    }
-                    stage('Functional Tests') {
-                        steps {
-                            sh "echo FUNCTIONAL TESTS Completed"
-                        }
-                    }
-                }
-            }
-        } // end of stages
-    }
-}
+//def call() {
+//    pipeline {
+//        agent any
+//        environment { SONAR = credentials('SONAR') }
+//        stages {
+//            stage('Lint Checks') {
+//                steps {
+//                    script { common.lintCheck() }
+//                }
+//            }
+//            stage('SonarScan') {
+//                steps {
+//                    script {
+//                        env.ARGS="-Dsonar.sources=."
+//                        common.sonarCheck() }
+//                }
+//            }
+//            stage('Test Cases') {
+//                parallel {
+//                    stage('Unit Tests') {
+//                        steps {
+//                            sh "echo UNIT TESTS Completed"
+//                        }
+//
+//                    }
+//                    stage('Integration Tests') {
+//                        steps {
+//                            sh "echo INTEGRATION TESTS Completed"
+//                        }
+//                    }
+//                    stage('Functional Tests') {
+//                        steps {
+//                            sh "echo FUNCTIONAL TESTS Completed"
+//                        }
+//                    }
+//                }
+//            }
+//        } // end of stages
+//    }
+//}
