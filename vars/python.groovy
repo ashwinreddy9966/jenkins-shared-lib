07c8f278decfcb26eb541689c5e97b08025a7a13
manu <manu@local.com>
2022-05-12 12:33:27 +0530

    Auto Push Commit

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,5 +1,7 @@
 def call() {
     node {
+        sh 'rm -rf *'
+        git branch: 'main', url: "https://github.com/raghudevopsb63/${COMPONENT}"
         env.APP_TYPE = "python"
         sh "ls -ltr"
         common.lintCheck()
