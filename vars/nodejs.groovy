3ba5c70b01cd1d78b3fcbd510c70ecd3214f2be5
manu <manu@local.com>
2022-05-15 09:54:56 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,7 @@
 def call() {
     node {
+        sh 'rm -rf *'
+        git branch: 'main', credentialsId: 'GitHub-Cred', url: "https://github.com/ashwinreddy9966/${COMPONENT}"
         env.APP_TYPE = "nodejs"
         common.lintCheck()
         env.ARGS="-Dsonar.sources=."
