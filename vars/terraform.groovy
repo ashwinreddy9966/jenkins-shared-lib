1f052d486bea0f53158400b14c65042de3cbe106
manu <manu@local.com>
2022-05-22 15:32:39 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -19,7 +19,7 @@ def call() {
             }
 
             stage('Terraform Apply') {
-                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+                sh "terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
             }
         }
     }
