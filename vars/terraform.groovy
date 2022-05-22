9de9a5ca5aecc66eb0f32a59ec567d0be85a78b4
manu <manu@local.com>
2022-05-22 15:30:32 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,25 +1,26 @@
 def call() {
-
     properties([
             parameters([
                     choice(choices: 'dev\nprod', description: "Select Environment", name: "ENV"),
             ]),
     ])
     node {
-        sh 'rm -rf *'
-        git branch: 'main', url: "https://github.com/ashwinreddy9966/${REPONAME}"
+        ansiColor('xterm') {
+            sh 'rm -rf *'
+            git branch: 'main', url: "https://github.com/ashwinreddy9966/${REPONAME}"
 
-        stage('Terraform Initialisization'){
-            sh "cp env-${ENV}/Terrafile . ; terrafile"
-            sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
-        }
+            stage('Terraform Initialisization') {
+                sh "cp env-${ENV}/Terrafile . ; terrafile"
+                sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
+            }
 
-        stage('Terraform Plan'){
-            sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
-        }
+            stage('Terraform Plan') {
+                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+            }
 
-        stage('Terraform Apply'){
-            sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+            stage('Terraform Apply') {
+                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+            }
         }
     }
 }
