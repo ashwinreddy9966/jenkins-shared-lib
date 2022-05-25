5518fff07057e6061cf44ffc23640a084b42055b
manu <manu@local.com>
2022-05-25 12:11:36 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -14,18 +14,18 @@ def call() {
             git branch: 'main', url: "https://github.com/ashwinreddy9966/${REPONAME}"
 
             stage('Terraform Initialisization') {
-                sh "cd ${TERRAFROM_DIR}"
+                sh "cd ${TERRAFORM_DIR}"
                 sh "cp env-${ENV}/Terrafile . ; terrafile"
                 sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
             }
 
             stage('Terraform Plan') {
-                sh "cd ${TERRAFROM_DIR}"
+                sh "cd ${TERRAFORM_DIR}"
                 sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
             }
 
             stage('Terraform Apply') {
-                sh "cd ${TERRAFROM_DIR}"
+                sh "cd ${TERRAFORM_DIR}"
                 sh "terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
             }
         }
