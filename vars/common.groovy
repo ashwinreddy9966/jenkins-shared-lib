ea5b411c1604745c38cbf23e046d3db904e6a403
manu <manu@local.com>
2022-06-11 22:28:25 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -93,7 +93,7 @@ def artifacts() {
         stage('Publishing AMI') {
             ansiColor('xterm') {
                 sh '''
-                    rm -rf .terraform
+                    
                     terraform init
                     terraform apply -auto-approve -var APP_VERSION=${TAG_NAME}
                 '''
