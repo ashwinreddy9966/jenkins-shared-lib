2f5cd872e68c3a14413eab7905e4de1bb07d48a7
manu <manu@local.com>
2022-06-14 10:44:16 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -10,7 +10,7 @@ def call() {
             stage('Docker Push') {
 
                 sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
-                sh "docker login -u AWS https://834725375088.dkr.ecr.us-east-1.amazonaws.com -p $(aws ecr get-login-password --region us-east-1)"
+                sh '''docker login -u AWS https://834725375088.dkr.ecr.us-east-1.amazonaws.com -p $(aws ecr get-login-password --region us-east-1)'''
                 sh "docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
             }
         }
