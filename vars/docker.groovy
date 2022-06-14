b088a8fff8f052b2da50cf7fdce3f486468c3842
manu <manu@local.com>
2022-06-14 10:11:36 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,8 +4,7 @@ def call() {
         git branch: 'main', credentialsId: 'GitHub-Cred', url: "https://github.com/ashwinreddy9966/${COMPONENT}"
 
         stage('Docker Build') {
-            sh "ls -ltr && pwd && whoami && hostname"
-            sh "docker build -t 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest "
+            sh "docker build -t 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
         }
     }
 }
