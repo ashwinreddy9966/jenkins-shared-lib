f6708df7b1033d561da7a78183b7dcd1351fa9bb
manu <manu@local.com>
2022-06-14 07:53:08 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -2,13 +2,9 @@ def call() {
     node {
         sh 'rm -rf *'
         git branch: 'main', credentialsId: 'GitHub-Cred', url: "https://github.com/ashwinreddy9966/${COMPONENT}"
-        env.APP_TYPE = "nodejs"
-        common.lintCheck()
-        env.ARGS="-Dsonar.sources=."
-        common.sonarCheck()
-        common.testCases()
-        if(env.TAG_NAME != null) {
-            common.artifacts()
+
+        stage('') {
+            sh "docker build . "
         }
     }
 }
