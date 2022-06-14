d5f7c2b9908bdde6b691ea38c51788e673178fc8
manu <manu@local.com>
2022-06-14 07:53:51 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -3,7 +3,7 @@ def call() {
         sh 'rm -rf *'
         git branch: 'main', credentialsId: 'GitHub-Cred', url: "https://github.com/ashwinreddy9966/${COMPONENT}"
 
-        stage('') {
+        stage('Docker Build') {
             sh "docker build . "
         }
     }
